8794febc0d1eae3a590f2364bdab9536dc04b19e
SUNG IL LEE <sungil.lee90@gmail.com>
2020-03-23 14:30:45 -0500

    Complete Exercise #0.

--- code.js
+++ code.js
@@ -1,4 +1,15 @@
 // helloWorld function 
-function helloWorld() {
+const helloWorld = function () {
     return "Hello, World!";
+
+    // console.log ("Hello, World!");
+    // When changing code from return to console.log
+    // should never return 'undefined' when called
+    // should return a string when called
+    // should return the string "Hello, World!" when executed
 }
+
+// when I changed helloWorld to hello, it says "un-used" function (Expected 'undefined' to be 'function'.)
+// Lastly change to function expression was implemented from function helloworld ()
+
+
